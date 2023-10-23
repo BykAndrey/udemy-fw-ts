71b0310fa37f4fe5998b56f9db51b6f4c9667d24
BykAndrey <andreybyk9606@gmail.com>
2023-10-23 23:27:43 +0300

    init linters

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('')
+import { User } from './models/User'
+
+const user = new User({ name: 'a', age: 20 })
+
+console.log(user.get('name'))
+console.log(user.get('age'))
+
+user.set({ name: 'b', age: 30 })
+
+console.log(user.get('name'))
+console.log(user.get('age'))
---
+++ src/models/User.ts
@@ -0,0 +1,19 @@
+interface UserProps {
+	name?: string;
+	age?: number;
+}
+
+type Callback = () => {};
+
+export class User {
+	private events: { [key: string]: Callback[] } = {};
+	constructor(private data: UserProps) {}
+	get(propName: string): string | number {
+		return this.data[propName];
+	}
+	set(update: UserProps): void {
+		Object.assign(this.data, update);
+	}
+	on(eventName: string, callback: Callback): void {}
+	trigger() {}
+}
