6c5d16c410c2f5299fdc86a19e06c6214faef457
BykAndrey <andreybyk9606@gmail.com>
2023-11-04 21:48:22 +0300

    collection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-import axios from 'axios';
+import { User } from "./models/User";
+
+const collection = User.buildCollection();
+
+collection.fetch();
+collection.on('change', () => {
+    console.log(collection.models)
+})
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,7 @@ import { ApiSync } from "./ApiSync";
 import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
 import { Model } from "./Model";
-
+import { Collection } from "./Collection";
 export interface UserProps {
 	id?: number;
 	name?: string;
@@ -10,12 +10,21 @@ export interface UserProps {
 }
 const rootUrl = 'http://localhost:3000/users';
 export class User extends Model<UserProps> {
-	static  buildUser(data: UserProps): User {
+	static buildUser(data: UserProps): User {
 		return new User(
 			new Attributes<UserProps>(data),
 			new ApiSync<UserProps>(rootUrl),
 			new Eventing()
 		)
 	}
- }
- 
+	static buildCollection(): Collection<User, UserProps> {
+		return new Collection<User, UserProps>(
+			rootUrl,
+			(value) => {
+				return User.buildUser(value);
+			}
+		);
+	}
+}
+
+
