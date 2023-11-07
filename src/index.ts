a9904f9262cd2df7a70d9e0863b1f87ca722bd88
BykAndrey <andreybyk9606@gmail.com>
2023-11-07 14:10:46 +0300

    collection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
-import { User } from "./models/User";
-import { UserEdit } from "./views/UserEdit";
+// import { User } from "./models/User";
+// import { UserEdit } from "./views/UserEdit";
 
 
-const user = User.buildUser({ name: 'Max', age: 10 });
-const form = new UserEdit(document.getElementById('root')!, user);
-form.render();
+// const user = User.buildUser({ name: 'Max', age: 10 });
+// const form = new UserEdit(document.getElementById('root')!, user);
+// form.render();
+
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+
+import { User, UserProps } from "./models/User";
+
+const collection = new Collection<User, UserProps>(
+    'http://localhost:3000/users',
+    (json:UserProps) => User.buildUser(json)
+);
+const parent = document.querySelector('#root')!;
+collection.on('change', () => {
+    new UserList(parent, collection).render();
+})
+collection.fetch();
